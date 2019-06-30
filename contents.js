82babc87fb27ae0fcbf25032c95b85fad8382b69
kuroneko423 <kuroneko.yk@gmail.com>
2019-06-30 11:33:37 +0900

    Add files via upload

--- contents.js
+++ contents.js
@@ -1,7 +1,19 @@
 window.onload = function () {
     console.log('contents.js start');
+    
+    var frame_num = window.length;
     var script = document.createElement('script');
     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
-    document.head.appendChild(script);
+    if (frame_num = 0) {
+        console.log('0');
+        document.head.appendChild(script);        
+    } else {
+        console.log(frame_num);
+        window.document.head.appendChild(script);  
+        for (let index = 0; index < frame_num; index++) {
+            window.frames[index].head.appendChild(script);
+        }
+    }
+//    document.head.appendChild(script);
     console.log('contents.js end');
 };
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-window.onload = function () {
-    console.log('main.js start');
+window.addEventListener("load", main, false);
+
+function main(e) {
+    var frame_num = window.length;
     var script = document.createElement('script');
     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
     document.head.appendChild(script);
-    console.log('main.js end');
-};
+    console.log('main.js finish');
+}
